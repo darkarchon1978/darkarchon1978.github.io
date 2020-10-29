cf73dbd25cc52240f6182e041018e14c8e232b0d
Zoz <darkarchon1978@yahoo.com>
2020-10-29 18:47:51 +0100

    login

---
+++ js/admin.js
@@ -0,0 +1,18 @@
+function getFireBaseData() {
+    let button = document.getElementById('get-div');
+    fade(button);
+    // FireBase lekérdezés ide
+}
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1) {
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 25);
+}
--- js/login.js
+++ js/login.js
@@ -1,16 +1,22 @@
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         // User is signed in
-        document.getElementById('user_div').style.display = "initial";
-        document.getElementById('login_div').style.display = "none";
+        document.getElementById('user-div').style.display = "initial";
+        document.getElementById('get-div').style.display = "initial";
+        document.getElementById('login-div').style.display = "none";
     } else {
         // No user is signed in
-        document.getElementById('user_div').style.display = "none";
-        document.getElementById('login_div').style.display = "initial";
+        document.getElementById('user-div').style.display = "none";
+        document.getElementById('get-div').style.display = "none";
+        document.getElementById('login-div').style.display = "initial";
     }
 });
 
-function login() {
+let showEmail = ''
+let mail = document.getElementById('usermail-div');
+mail.innerText = JSON.parse(sessionStorage['showEmail'].toString());
+
+function logIn() {
     let userEmail = document.getElementById("email-field").value;
     let userPassword = document.getElementById("password-field").value;
     firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function (error) {
@@ -19,7 +25,17 @@ function login() {
         var errorMessage = error.message;
 
         alert('Hiba: ' + errorMessage);
+    });
+    sessionStorage['showEmail'] = JSON.stringify(userEmail);
+    let mail = document.getElementById('usermail-div');
+    showEmail = userEmail;
+    mail.innerText = userEmail;
+}
 
-        // ...
+function logOut() {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+    }).catch(function (error) {
+        // An error happened.
     });
 }
