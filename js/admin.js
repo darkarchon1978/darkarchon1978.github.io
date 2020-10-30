8a72f7066efee5c8098087614a2173f8423e9c57
Zoz <darkarchon1978@yahoo.com>
2020-10-30 17:15:49 +0100

    admin page layout

--- js/admin.js
+++ js/admin.js
@@ -2,6 +2,7 @@ function getFireBaseData() {
     var productsArray = [];
     const db = firebase.firestore();
     setFields();
+    document.getElementById('adminTable').style.display = 'table';
     db.collection("products").get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
             var arr = [];
@@ -17,9 +18,15 @@ function getFireBaseData() {
             outputAdminHTML += `
                 <tr>
                     <td class="text-center align-middle">
+                    <div class="btn-group">
                     <button class="btn btn-sm btn-danger" data-action="DELETE_ITEM">
-                    <i class="far fa-trash-alt align-middle" style="font-size: 18px;"></i>
-                    </button></td>
+                    <i class="far fa-trash-alt align-middle" style="font-size: 24px;"></i>
+                    </button>
+                    <button class="btn btn-sm btn-info" data-action="EDIT_ITEM">
+                    <i class="far fa-edit" style="font-size: 24px;"></i>
+                    </button>
+                    </div>
+                    </td>
                     <td class="text-center align-middle">${value.id}</td>
                     <td><img src="img/list/${value.mainImage}" class="adminTableImage" alt="${value.name}"></td>
                     <td class="text-center align-middle">${value.name}</td>
@@ -28,7 +35,7 @@ function getFireBaseData() {
                     <td class="text-center align-middle">${value.price}</td>
                     <td class="text-center align-middle">${value.numberOfImages}</td>
                 </tr>`
-            
+
         })
         $('#outputAdmin').html(outputAdminHTML);
 
@@ -37,22 +44,73 @@ function getFireBaseData() {
             alert('Ezt most ne!')
             // Modal kezelés - biztos-e benne?
         });
+
+
     });
 
 }
 
-{/* <th class="text-center">Törlés</th>
-            <th class="text-center">Kép</th>
-            <th class="text-center">Megnevezés</th>
-            <th class="text-center">Leírás</th>
-            <th class="text-center">Mottó</th>
-            <th class="text-center">Ár</th>
-            <th class="text-center">Képek Száma</th> */}
+// Adatfelvitel HTML létrehozása
+function createFireBaseData() {
+    setFields();
+    let createProductHTML = `
+    <div class="form-row">
+    <div class="form-group col-md-2">
+        
+        <div class="input-group">
+            <input type="text" placeholder="Cikkszám" id="inputID" class="form-control">
+            <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button">
+            <i class="far fa-question-circle" style="font-size: 24px;"></i></button>    
+            </div>
+        </div>
+    </div>
+    <div class="form-group col-md-8">
+        
+        <input placeholder="Megnevezés" type="text" class="form-control" id="inputName">
+    </div>
+    <div class="form-group col-md-2">
+    
+    <div class="input-group">
+  <div class="input-group-prepend">
+    <label class="input-group-text" for="inputNumberOfImages">Képek</label>
+  </div>
+  <select class="custom-select" id="inputNumberOfImages">
+    <option selected>0</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+  </select>
+</div>
+        
+        
+    </div>
+</div>
+<div class="form-row">
+    <div class="form-group col-md-2">
+    
+        <input placeholder="Ár" type="text" class="form-control" id="inputPrice">
+    </div>
+
+    <div class="form-group col-md-10">
+    
+        <input placeholder="Mottó" type="text" class="form-control" id="inputMotto">
+    </div>
+</div>
+<div class="form-group">
+ 
+    <textarea placeholder="Leírás" class="form-control" id="inputDescription"
+        rows="5"></textarea>
+    </div>
+    <div id="uploadButton" class="btn btn-block btn-success">FELTÖLTÉS</div>`
+    $('#createProduct').html(createProductHTML);
+}
 
 function setFields() {
     document.getElementById('get-div').style.display = "none";
     document.getElementById('filler-left').classList.replace('col-md-4', 'col-xl-2');
     document.getElementById('maincontent').classList.replace('col-md-4', 'col-xl-8');
     document.getElementById('filler-right').classList.replace('col-md-4', 'col-xl-2');
-    document.getElementById('adminTable').style.display = 'table';
 }
