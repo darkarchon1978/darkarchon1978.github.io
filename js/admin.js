e510c72563f556b5a797e877ce3d0a95037b13af
Zoz <darkarchon1978@yahoo.com>
2020-11-01 17:24:18 +0100

    admin gombkezelés

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,6 @@
+'use strict'
+
+// GET DATA BUTTON FUNCTIONS
 function getFireBaseData() {
     var productsArray = [];
     const db = firebase.firestore();
@@ -11,7 +14,7 @@ function getFireBaseData() {
             Object.assign(arr, id);
             productsArray.push(arr);
         });
-        // Táblázatsorok generálása
+        // GENERATING TABLE ROWS
         let outputAdminHTML = '';
         $.each(productsArray, function (index, value) {
             ++index;
@@ -20,10 +23,10 @@ function getFireBaseData() {
                     <td class="text-center align-middle">
                     <div class="btn-group">
                     <button class="btn btn-sm btn-danger" data-action="DELETE_ITEM">
-                    <i class="far fa-trash-alt align-middle" style="font-size: 24px;"></i>
+                    <i class="far fa-trash-alt align-middle" style="font-size: 20px;"></i>
                     </button>
                     <button class="btn btn-sm btn-info" data-action="EDIT_ITEM">
-                    <i class="far fa-edit" style="font-size: 24px;"></i>
+                    <i class="far fa-edit" style="font-size: 20px;"></i>
                     </button>
                     </div>
                     </td>
@@ -39,10 +42,10 @@ function getFireBaseData() {
         })
         $('#outputAdmin').html(outputAdminHTML);
 
-        // Törlés kezelés
+        // HANDLING DELETION
         $('#outputAdmin').on('click', '[data-action="DELETE_ITEM"]', function () {
             alert('Ezt most ne!')
-            // Modal kezelés - biztos-e benne?
+            // MODAL HANDLING - ARE YOU SURE?
         });
 
 
@@ -50,21 +53,16 @@ function getFireBaseData() {
 
 }
 
-// Adatfelvitel HTML létrehozása
+// UPLOAD NEW DATA BUTTON FUNCTIONS
 function createFireBaseData() {
+    let id = (Math.floor(100000 + Math.random() * 900000));
     setFields();
     let createProductHTML = `
     <div class="form-row">
     <div class="form-group col-md-2">
-        
-        <div class="input-group">
-            <input type="text" placeholder="Cikkszám" id="inputID" class="form-control">
-            <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button">
-            <i class="far fa-question-circle" style="font-size: 24px;"></i></button>    
-            </div>
-        </div>
+            <input type="text" class="form-control" id="inputID" value="${id}">
     </div>
+
     <div class="form-group col-md-8">
         
         <input placeholder="Megnevezés" type="text" class="form-control" id="inputName">
@@ -104,13 +102,80 @@ function createFireBaseData() {
     <textarea placeholder="Leírás" class="form-control" id="inputDescription"
         rows="5"></textarea>
     </div>
-    <div id="uploadButton" class="btn btn-block btn-success">FELTÖLTÉS</div>`
+    <button id="uploadButton" onclick="startUpload()" class="btn btn-block btn-success">TERMÉK RÖGZÍTÉSE</button>`
     $('#createProduct').html(createProductHTML);
+    checkID();
+    $("#inputID").change(function () {
+        checkID()
+    });
 }
 
 function setFields() {
-    document.getElementById('get-div').style.display = "none";
+    $("#get-div").hide();
     document.getElementById('filler-left').classList.replace('col-md-4', 'col-xl-2');
     document.getElementById('maincontent').classList.replace('col-md-4', 'col-xl-8');
     document.getElementById('filler-right').classList.replace('col-md-4', 'col-xl-2');
+}
+
+function startUpload() {
+    console.log('Feltöltés...')
+}
+
+function checkID() {
+    let inputIDValue = document.getElementById('inputID').value;
+    if (inputIDValue.length != 6) {
+        setInvalid();
+        alert('A cikkszám nem hatjegyű!')
+    } else if (isNaN(inputIDValue)) {
+        setInvalid();
+        alert('A cikkszám nem csak számokból áll!')
+    } else {
+        let docIDArray = [];
+        let matchID = false;
+        const db = firebase.firestore();
+        db.collection("products").get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                docIDArray.push(doc.id);
+            })
+            for (let i = 0; i < docIDArray.length; i++) {
+                if (docIDArray[i] === inputIDValue) {
+                    matchID = true;
+                }
+            }
+            if (matchID === true) {
+                // CIKKSZÁM LÉTEZIK
+                setOverwrite();
+            } else {
+                // CIKKSZÁM NEM LÉTEZIK
+                setValid();
+            }
+        });
+    };
+}
+
+function setInvalid() {
+    $("#inputID").addClass("is-my-invalid");
+    $("#inputID").removeClass("is-valid");
+    $("#inputID").removeClass("is-overwrite");
+    $('#uploadButton').prop('disabled', true);
+    $('#uploadButton').removeClass('btn-warning');
+    $('#uploadButton').text('ÉRVÉNYTELEN ADATOK');
+}
+
+function setValid() {
+    $("#inputID").addClass("is-valid");
+    $("#inputID").removeClass("is-overwrite");
+    $("#inputID").removeClass("is-my-invalid");
+    $('#uploadButton').prop('disabled', false);
+    $('#uploadButton').removeClass('btn-warning');
+    $('#uploadButton').text('TERMÉK RÖGZÍTÉSE');
+}
+function setOverwrite() {
+    $("#inputID").removeClass("is-valid");
+    $("#inputID").addClass("is-overwrite");
+    $("#inputID").removeClass("is-my-invalid");
+    alert('Létező cikkszám, a termék minden adata felülírásra kerül!');
+    $('#uploadButton').prop('disabled', false);
+    $('#uploadButton').text('TERMÉK FELÜLÍRÁSA');
+    $('#uploadButton').addClass('btn-warning');
 }
