df35b021a32468d11cc4aa7c8fef4e2eb73bc360
Zoz <darkarchon1978@yahoo.com>
2020-11-02 16:37:17 +0100

    upload fields finalized

--- js/admin.js
+++ js/admin.js
@@ -53,61 +53,48 @@ function getFireBaseData() {
 
 }
 
+let inputIDState = 'invalid';
+let inputNameState = false;
+let inputDescriptionState = false;
+let inputPriceState = false;
+
 // UPLOAD NEW DATA BUTTON FUNCTIONS
 function createFireBaseData() {
-    let id = (Math.floor(100000 + Math.random() * 900000));
     setFields();
-    let createProductHTML = `
-    <div class="form-row">
-    <div class="form-group col-md-2">
-            <input type="text" class="form-control" id="inputID" value="${id}">
-    </div>
-
-    <div class="form-group col-md-8">
-        
-        <input placeholder="Megnevezés" type="text" class="form-control" id="inputName">
-    </div>
-    <div class="form-group col-md-2">
-    
-    <div class="input-group">
-  <div class="input-group-prepend">
-    <label class="input-group-text" for="inputNumberOfImages">Képek</label>
-  </div>
-  <select class="custom-select" id="inputNumberOfImages">
-    <option selected>0</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-  </select>
-</div>
-        
-        
-    </div>
-</div>
-<div class="form-row">
-    <div class="form-group col-md-2">
-    
-        <input placeholder="Ár" type="text" class="form-control" id="inputPrice">
-    </div>
-
-    <div class="form-group col-md-10">
-    
-        <input placeholder="Mottó" type="text" class="form-control" id="inputMotto">
-    </div>
-</div>
-<div class="form-group">
- 
-    <textarea placeholder="Leírás" class="form-control" id="inputDescription"
-        rows="5"></textarea>
-    </div>
-    <button id="uploadButton" onclick="startUpload()" class="btn btn-block btn-success">TERMÉK RÖGZÍTÉSE</button>`
-    $('#createProduct').html(createProductHTML);
+    createUploadFields();
+    checkFieldsState();
     checkID();
-    $("#inputID").change(function () {
-        checkID()
+    checkName();
+    checkPrice();
+    showCharCount();
+    $("#inputID").keyup(function () {
+        checkID();
+        // checkFieldsState(); <- a checkID funkción belül van
     });
+    $("#inputName").keyup(function () {
+        checkName();
+        checkFieldsState();
+    });
+    $("#inputDescription").keyup(function () {
+        showCharCount();
+        checkFieldsState();
+    });
+    $("#inputPrice").keyup(function () {
+        checkPrice();
+        checkFieldsState();
+    });
+}
+
+function checkFieldsState() {
+    if (inputIDState === 'new' && inputNameState === true && inputDescriptionState === true && inputPriceState === true) {
+        handleButton('success', 'enabled');
+    } else if (inputIDState === 'overwrite' && inputNameState === true && inputDescriptionState === true && inputPriceState === true) {
+        handleButton('warning', 'enabled');
+    } else if (inputIDState === 'overwrite') {
+        handleButton('warning', 'disabled');
+    } else if (inputIDState === 'invalid' || inputNameState === false || inputDescriptionState === false || inputPriceState === false) {
+        handleButton('danger', 'disabled');
+    }
 }
 
 function setFields() {
@@ -121,14 +108,49 @@ function startUpload() {
     console.log('Feltöltés...')
 }
 
+function checkPrice() {
+    let inputPriceValue = document.getElementById('inputPrice').value;
+    if (isNaN(inputPriceValue) || inputPriceValue === '') {
+        inputPriceState = false;
+        setFieldInvalid('#inputPrice')
+    } else {
+        inputPriceState = true;
+        setFieldValid('#inputPrice')
+    }
+}
+
+function checkName() {
+    let inputNameValue = document.getElementById('inputName').value;
+    let inputDescriptionValue = document.getElementById('inputDescription').value;
+    if (inputNameValue.length < 3) {
+        inputNameState = false;
+        setFieldInvalid('#inputName')
+    } else {
+        inputNameState = true;
+        setFieldValid('#inputName')
+    }
+
+    if (inputDescriptionValue.length < 50) {
+        inputDescriptionState = false;
+        setFieldInvalid('#inputDescription')
+    } else {
+        inputDescriptionState = true;
+        setFieldValid('#inputDescription')
+    }
+
+}
+
+
 function checkID() {
     let inputIDValue = document.getElementById('inputID').value;
     if (inputIDValue.length != 6) {
-        setInvalid();
-        alert('A cikkszám nem hatjegyű!')
+        inputIDState = 'invalid';
+        setFieldInvalid('#inputID');
+        checkFieldsState();
     } else if (isNaN(inputIDValue)) {
-        setInvalid();
-        alert('A cikkszám nem csak számokból áll!')
+        inputIDState = 'invalid';
+        setFieldInvalid('#inputID');
+        checkFieldsState();
     } else {
         let docIDArray = [];
         let matchID = false;
@@ -144,38 +166,145 @@ function checkID() {
             }
             if (matchID === true) {
                 // CIKKSZÁM LÉTEZIK
-                setOverwrite();
+                inputIDState = 'overwrite';
+                setFieldOverwrite('#inputID');
             } else {
                 // CIKKSZÁM NEM LÉTEZIK
-                setValid();
+                inputIDState = 'new';
+                setFieldValid('#inputID');
             }
+            checkFieldsState();
         });
     };
 }
 
-function setInvalid() {
-    $("#inputID").addClass("is-my-invalid");
-    $("#inputID").removeClass("is-valid");
-    $("#inputID").removeClass("is-overwrite");
-    $('#uploadButton').prop('disabled', true);
-    $('#uploadButton').removeClass('btn-warning');
-    $('#uploadButton').text('ÉRVÉNYTELEN ADATOK');
+function showCharCount() {
+    let inputDescriptionValueLength = document.getElementById('inputDescription').value.length;
+    if (inputDescriptionValueLength >= 50) {
+        $('#charCount').text('meg van az 50.');
+        inputDescriptionState = true;
+        setFieldValid('#inputDescription');
+    } else {
+        inputDescriptionState = false;
+        setFieldInvalid('#inputDescription');
+        $('#charCount').text('még ' + (50 - inputDescriptionValueLength) + ' karakter kell.');
+    }
 }
 
-function setValid() {
-    $("#inputID").addClass("is-valid");
-    $("#inputID").removeClass("is-overwrite");
-    $("#inputID").removeClass("is-my-invalid");
-    $('#uploadButton').prop('disabled', false);
-    $('#uploadButton').removeClass('btn-warning');
-    $('#uploadButton').text('TERMÉK RÖGZÍTÉSE');
+function handleButton(color, state) {
+    switch (color + "-" + state) {
+        // initial; invalid data
+        case 'danger-disabled': {
+            $('#uploadButton').prop('disabled', true);
+            $('#uploadButton').removeClass('btn-warning');
+            $('#uploadButton').removeClass('btn-success');
+            $('#uploadButton').addClass('btn-danger');
+            $('#uploadButton').text('ÉRVÉNYTELEN ADATOK');
+            break;
+        }
+        // overwrite; invalid data
+        case 'warning-disabled': {
+            $('#uploadButton').prop('disabled', true);
+            $('#uploadButton').text('TERMÉK FELÜLÍRÁSA');
+            $('#uploadButton').addClass('btn-warning');
+            $('#uploadButton').removeClass('btn-success');
+            $('#uploadButton').removeClass('btn-danger');
+            break;
+        }
+        // overwrite; valid data
+        case 'warning-enabled': {
+            $('#uploadButton').prop('disabled', false);
+            $('#uploadButton').text('TERMÉK FELÜLÍRÁSA');
+            $('#uploadButton').addClass('btn-warning');
+            $('#uploadButton').removeClass('btn-success');
+            $('#uploadButton').removeClass('btn-danger');
+            break;
+        }
+        // valid data
+        case 'success-enabled': {
+            $('#uploadButton').prop('disabled', false);
+            $('#uploadButton').removeClass('btn-warning');
+            $('#uploadButton').addClass('btn-success');
+            $('#uploadButton').removeClass('btn-danger');
+            $('#uploadButton').text('TERMÉK RÖGZÍTÉSE');
+            break;
+        }
+        default:
+            alert('Hiba a kódban. Kérem, forduljon a rendszergazdához!')
+    }
 }
-function setOverwrite() {
-    $("#inputID").removeClass("is-valid");
-    $("#inputID").addClass("is-overwrite");
-    $("#inputID").removeClass("is-my-invalid");
-    alert('Létező cikkszám, a termék minden adata felülírásra kerül!');
-    $('#uploadButton').prop('disabled', false);
-    $('#uploadButton').text('TERMÉK FELÜLÍRÁSA');
-    $('#uploadButton').addClass('btn-warning');
+
+function setFieldInvalid(field) {
+    $(field).addClass("is-invalid");
+    $(field).removeClass("is-valid");
+    $(field).removeClass("is-overwrite");
+}
+
+function setFieldValid(field) {
+    $(field).addClass("is-valid");
+    $(field).removeClass("is-overwrite");
+    $(field).removeClass("is-invalid");
+}
+function setFieldOverwrite(field) {
+    $(field).removeClass("is-valid");
+    $(field).addClass("is-overwrite");
+    $(field).removeClass("is-invalid");
+    // setTimeout(function () { alert('Létező cikkszám, a termék minden adata felülírásra kerül!'); }, 250);
+}
+
+function createUploadFields() {
+    let randomID = (Math.floor(100000 + Math.random() * 900000));
+    let createProductHTML = `
+<div class="form-row">
+<div class="form-group col-md-2">
+        <input type="text" class="form-control" id="inputID" value="${randomID}">
+        <small class="form-text text-muted">hatszámjegyű cikkszám</small>
+</div>
+
+<div class="form-group col-md-8">
+    
+    <input placeholder="Megnevezés" type="text" class="form-control" id="inputName">
+    <small class="form-text text-muted">minimum 3 karakter</small>
+</div>
+<div class="form-group col-md-2">
+
+<div class="input-group">
+<div class="input-group-prepend">
+<label class="input-group-text" for="inputNumberOfImages">Képek</label>
+</div>
+<select class="custom-select" id="inputNumberOfImages">
+<option selected>0</option>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+</select>
+</div>
+
+<small class="form-text text-muted">extra képek száma</small>
+    
+</div>
+</div>
+<div class="form-row">
+<div class="form-group col-md-2">
+
+    <input placeholder="Ár" type="text" class="form-control" id="inputPrice">
+    <small class="form-text text-muted">csak számjegyek</small>
+</div>
+
+<div class="form-group col-md-10">
+
+    <input placeholder="Mottó" type="text" class="form-control" id="inputMotto">
+    <small class="form-text text-muted">(nem kötelező)</small>
+</div>
+</div>
+<div class="form-group">
+
+<textarea placeholder="Leírás" class="form-control text-justify" id="inputDescription"
+    rows="8"></textarea>
+    <small class="form-text text-muted">minimum 50 karakter: <span id="charCount"></span></small>
+</div>
+<button id="uploadButton" onclick="startUpload()" class="btn btn-block btn-success">TERMÉK RÖGZÍTÉSE</button>`
+    $('#createProduct').html(createProductHTML);
 }
